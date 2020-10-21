a5e51999a9ebfa0d14758c9de3eee7a9f0be141d
Ashley Campbell <ashleycampbell@MacBook-Pro.lan>
2020-10-21 23:31:44 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,120 @@
+/*- Gives the name input focus immedietely -*/
+document.querySelector('#name').focus();
+/*- ------------------------------------- -*/
+/*- Shows 'other job input' if 'other' is selected for job role -*/
+const otherJobDiv = document.querySelector('.job_other');
+const jobRoleInput = document.querySelector('#title');
+jobRoleInput.addEventListener('change', (e) => {
+    if (e.target.value == 'other') {
+        otherJobDiv.style.display = 'block';
+    } else {
+        otherJobDiv.style.display = 'none';
+    }
+});
+/*- ------------------------------------- -*/
+/*- ------------------------------------- -*/
+/*-- t-shirt info functionality --*/
+const tshirtDesignInput = document.querySelector('#design');
+const tshirtColorInput = document.querySelector('#color');
+const colourOptions = document.querySelectorAll('#color option');
+tshirtColorInput.value = 'noTheme';
+tshirtColorInput.disabled = true;
+
+// Simple function that enables the select list and adjusts the field.
+const updateTshirtField = (tshirt) => {
+    tshirtColorInput.disabled = false;
+    tshirtColorInput.value = tshirt;
+};
+// event listener that changes the list based on the tshirt design that the user has selected.
+tshirtDesignInput.addEventListener('change', (e) => {
+    colourOptions.forEach(colour => {
+        colour.style.display = '';
+    }); 
+    if (e.target.value === 'js puns') {
+        updateTshirtField('cornflowerblue');
+        colourOptions.forEach((colour, index) => {
+            if (index > 3 || index == 0) {
+                colour.style.display = 'none';
+            }
+    })     
+    } else if (e.target.value === 'Select Theme') {
+        tshirtColorInput.disabled = true;
+        tshirtColorInput.value = 'noTheme';
+    } else {
+        updateTshirtField('tomato');
+        colourOptions.forEach((colour, index) => {
+            if (index <= 3) {
+                colour.style.display = 'none';
+            }
+        }); 
+    }
+});
+/*- ------------------------------------- -*/
+/*- ------------------------------------- -*/
+/*-- ”Register for Activities” section --*/
+const tuesdayMorningEvents = document.querySelectorAll('input[data-day-and-time="Tuesday 9am-12pm"]');
+const tuesdayNoonEvents = document.querySelectorAll('input[data-day-and-time="Tuesday 1pm-4pm"]');
+const activityInputs = document.querySelectorAll('.activities input');
+const costSpan = document.querySelector('#workshop_cost');
+
+// function that disables a checkbox option and shows the hidden message
+const disableOption = (option) => {
+    option.disabled = true;
+    option.closest('label').style.color = 'gray';
+    option.nextElementSibling.style.display = 'block';
+}
+// function that enables a checkbox option and hides the hidden message
+const enableOption = (option) => {
+    option.disabled = false;
+    option.closest('label').style.color = 'black';
+    option.nextElementSibling.style.display = 'none';
+}
+// this function checks for event clashes. then calls the relevant function to hide or show the option.
+const eventClash = (input, index, array) => {
+    if(index == 0 && input.checked) {
+        disableOption(array[1]);
+    }
+    else if (index == 1 && input.checked) {
+        disableOption(array[0]);
+    }
+    else if(index == 0 && !input.checked) {
+        enableOption(array[1]);
+    }
+    else if (index == 1 && !input.checked) {
+        enableOption(array[0]);
+    }
+}
+// gets the total cost of all the events that are checked.
+let cost = 0;
+const getTotalCost = () => {
+    cost = 0;
+    activityInputs.forEach(input => {
+        if (input.checked) {
+            cost += parseInt(input.getAttribute('data-cost'));
+        }
+    })  
+    return cost;
+};
+
+const updateCost = (cost) => {
+    costSpan.textContent= ` $${cost}`;
+}
+
+// event listeners for the events that may clash with each other & for adding the cost of workshops
+tuesdayMorningEvents.forEach((input, index, array) => {
+    input.addEventListener('change', () => {
+      eventClash(input, index, array);
+    })
+})
+tuesdayNoonEvents.forEach((input, index, array) => {
+    input.addEventListener('change', () => {
+        eventClash(input, index, array);
+    })
+})
+activityInputs.forEach((input) => {
+    input.addEventListener('change', () => {
+        updateCost(getTotalCost()); // pass the cost of all the checked workshops to the updatecost function which updates the total cost span.
+    })
+})
+/*- ------------------------------------- -*/
+/*- ------------------------------------- -*/
